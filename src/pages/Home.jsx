b32caea14e3e0ab3c6d25c775d4cd4211dce5a42
TomasRodriguez30 <rodrigueztomas.dev@gmail.com>
2024-06-06 21:01:31 -0300

    Pantalla Home terminada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,11 +9,27 @@ function HomePage() {
   return (
     <>
       <Container maxWidth="lg">
-        <Box sx={{ color: '#ffffff' }}>
-          <Typography variant="h4" align="center" gutterBottom>
-            Acerca de mi
+        <Box sx={{ color: '#ffffff', textAlign: 'center' }}>
+          <Typography variant="h4" align="center" gutterBottom sx={{mb: 4}}>
+            Bienvenidos a Mi Portfolio 
           </Typography>
-          <Typography variant="body1" align="center" paragraph>
+        </Box>
+      </Container>
+      <Container maxWidth="lg">
+        <Box sx={{ color: '#ffffff', textAlign: 'center' }}>
+          <img src="../../assets/Foto3.jpg" alt="Foto de Perfil" style={{ width: '200px', height: '200px', borderRadius: '50%', objectFit: 'cover' }} />
+          <Typography variant="h4" align="center" gutterBottom sx={{mt: 4}}>
+            Tomás Javier Rodriguez Ocampo 
+          </Typography>
+          <img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif"></img>
+        </Box>
+      </Container>
+      <Container maxWidth="lg">
+        <Box sx={{ color: '#ffffff', textAlign: 'center' }}>
+          <Typography variant="h4" align="center" gutterBottom sx={{mt: 4}}>
+           <img src = "https://github.com/TomasRodriguez-dev/TomasRodriguez-dev/assets/102562595/d77f840c-70ed-4447-b139-f4e35eb27d2b" width="50px"></img> Acerca de mi
+          </Typography>
+          <Typography variant="body1" align="center" paragraph sx={{mb: 4, mt: 4}}>
             👨🏽‍🎓 Soy recibido en Técnico en Programación.
             <br />
             ✏️ Actualmente estoy estudiando Desarrollo de Software Full Stack.
@@ -26,10 +42,11 @@ function HomePage() {
             <br />
             ⏱️ Estoy ansioso por poder contribuir con mi experiencia y habilidades a un entorno dinámico y desafiante.
           </Typography>
+          <img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif"></img>
         </Box>
       </Container>
       <Container maxWidth="lg">
-        <Box sx={{ color: '#ffffff', mt: 6 }}>
+        <Box sx={{ color: '#ffffff' }}>
           <Typography variant="h4" align="center" gutterBottom>
             Habilidades
           </Typography>
@@ -38,7 +55,7 @@ function HomePage() {
           </Typography>
           <Grid container spacing={4}>
             {cardData.map((card, index) => (
-              <Grid item xs={12} sm={6} md={4} key={index}>
+              <Grid item xs={12} sx={{mb: 4, mt: 4}} sm={6} md={4} key={index}>
                 <Cards
                   title={card.title}
                   description={card.description}
